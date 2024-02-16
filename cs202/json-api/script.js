36eb548e68a840c872fbb4bfeba0a0beed6ac82a
Joshua Bishop <bishopj8@wwu.edu>
2024-02-15 18:33:03 -0800

    updating site pages

---
+++ cs202/json-api/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $.getJSON('http://api.open-notify.org/astros.json', function(data) {
+        $('#number-of-people').text(data['number'] + ' people are in space right now');
+        var astronauts = data['people'];
+        var astronautsList = '<h2>Who is in space?</h2>';
+        astronauts.forEach(function(astronaut) {
+            astronautsList += '<div class="astronaut">' + astronaut['name'] + ' on the ' + astronaut['craft'] + '</div>';
+        });
+        $('#astronauts-list').html(astronautsList);
+    }).fail(function(){
+        console.log("An error has occurred.");
+        $('#number-of-people').text('Failed to load data.');
+        $('#astronauts-list').text('');
+    });
+});
